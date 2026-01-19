75b113d21fc9e69b470d59cfcb5b6a7c5b0cd1be
Marco Kunz <6626615+wrthwhl@users.noreply.github.com>
2026-01-19 15:14:09 +0100

    fix: resolve console lint issues

--- apps/console/eslint.config.cjs
+++ apps/console/eslint.config.cjs
@@ -1,12 +1,9 @@
 const nextEslintPluginNext = require('@next/eslint-plugin-next');
-const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
 
 module.exports = [
   { plugins: { '@next/next': nextEslintPluginNext } },
-
   ...baseConfig,
-  ...nx.configs['flat/react-typescript'],
   {
     ignores: ['.next/**/*'],
   },
